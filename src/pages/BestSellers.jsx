27dbd56eef594d8c0361624f2faf83ae3e48baf3
joaovitorS-silva <joaovitu9659@gmail.com>
2026-08-18 22:05:03 -0300

    feat(BestSellers):add sidebar

--- src/pages/BestSellers.jsx
+++ src/pages/BestSellers.jsx
@@ -1,6 +1,7 @@
 import pc from "/src/assets/pc.png";
 import CardProduto from "../componentes/cardProduto";
-
+import { SquareCheckBig ,SquareDashed, SquareOff } from "lucide-react";
+import { useState } from "react";
 function BestSellers() {
   const produtos = [
     {
@@ -40,23 +41,43 @@ function BestSellers() {
       imagem: pc,
     },
   ];
+  const Marcas = ["apple", "samsung", "smeg", "asus"];
+  const [marcarsSelecionada, setmarcarsSelecionadas] = useState([]);
+
+  function OncheckBox(marca) {
+    setmarcarsSelecionadas((estadoAnterior) =>
+      estadoAnterior.includes(marca)
+        ? estadoAnterior.filter((m) => m !== marca)
+        : [...estadoAnterior, marca],
+    );
+  }
 
   return (
-    <section className="relative bg-[#0D0D0D] min-h-screen px-4 py-8 sm:px-6 lg:px-8">
-      <div className=" left-0 top-20 bg-red-500 h-full">iuhiuhui</div>
-      <div className="mx-auto max-w-7xl">
-        <div className="mb-6">
-          <h2 className="text-2xl font-bold text-white sm:text-3xl">
-            futuro bar de tipo de eletronico......
-          </h2>
+    <section className="  bg-[#0D0D0D] min-h-screen px-4 py-8 sm:px-6 lg:px-8">
+      <div className=" flex ">
+        <div className="w-64 shrink-0 p-6">
+          <h1 className="text-4xl text-center ">BESTSELLERS</h1>
+          <h2 className="text-white text-3xl mt-14">brand</h2>
+          <ul className=" flex flex-col mt-7 gap-4 text-white">
+            {Marcas.map((marca) => (
+              <li key={marca} className="flex gap-3">
+                <button type="button" onClick={() => OncheckBox(marca)}>
+                  {marcarsSelecionada.includes(marca) ? (
+                    <SquareOff />
+                  ) : (
+                    <SquareDashed />
+                  )}
+                </button>
+                {marca}
+              </li>
+            ))}
+          </ul>
         </div>
-
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 xl:grid-cols-3">
+        <div className="flex-1 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6 p-6">
           {produtos.map((produto) => (
             <CardProduto key={produto.id} produto={produto} />
           ))}
         </div>
-        <div className="bg-red-500 h-full"></div>
       </div>
     </section>
   );
